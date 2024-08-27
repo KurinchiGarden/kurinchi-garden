750500b341e164870f34ddc8d204524bb98cd6e7
h-a-r-i-code <hariharanarjunan@gmail.com>
2024-08-27 12:15:28 +0530

    created react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Kurinchi Garden</h1>
+        <p>Real estate details will come soon...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
